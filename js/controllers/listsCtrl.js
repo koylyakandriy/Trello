8d3a2b8e154e60e09ba9a00f39f80ab6d3695f2f
koylyakandriy <koylyakandriy@gmail.com>
2018-10-28 12:25:20 +0200

    To do list Trello

---
+++ js/controllers/listsCtrl.js
@@ -0,0 +1,9 @@
+angular.module('app').controller('listsCtrl', function (listFactory){
+	this.lists = listFactory.getLists();
+
+	this.addList = function () {
+		listFactory.addList(this.listName);
+		this.listName = '';
+	};
+
+});
